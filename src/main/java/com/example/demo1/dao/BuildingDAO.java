4f235e0d09caad2b6611f90418b6e6aba12a7501
mishanya2k <103316910+mishanya2k@users.noreply.github.com>
2023-05-25 16:23:29 +1000

    v 2.03

--- src/main/java/com/example/demo1/dao/BuildingDAO.java
+++ src/main/java/com/example/demo1/dao/BuildingDAO.java
@@ -55,14 +55,12 @@ public class BuildingDAO {
         this.towerDefender = crossbowman;
     }
     public void buyArcher(Archer archer){
-        if (player.getMoney() >= archer.getPrice()) {
             player.setMoney(player.getMoney() - archer.getPrice());
-        }
+
     }
     public void buyCrossbowman(Crossbowman crossbowman){
-        if (player.getMoney() >= crossbowman.getPrice()) {
             player.setMoney(player.getMoney() - crossbowman.getPrice());
-        }
+
     }
 
     public void sellArcher(Archer archer){
